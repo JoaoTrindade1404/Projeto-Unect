15f1ecb56ba19d008c06e3aee29f7c6734d20ea1
Joao <joaotrindade430@gmail.com>
2025-03-18 17:36:48 -0300

    js implementado

---
+++ src/script.js
@@ -0,0 +1,29 @@
+const slides = document.querySelectorAll('.slider');
+const elipses = document.querySelectorAll('.ellipse')
+let currentIndex = 0;
+
+function updateEllipse() {
+    elipses.forEach((ellipse, index) => {
+        if(index === currentIndex) {
+            ellipse.src="./assets/Ellipse 1.png";
+        }
+        else {
+            ellipse.src="./assets/Ellipse 2.png";
+        }
+    });
+}
+
+function changeSlide(direction) {
+    slides[currentIndex].classList.remove('on');
+    currentIndex += direction;
+    if (currentIndex < 0) {
+        currentIndex = slides.length - 1; 
+    } else if (currentIndex >= slides.length) {
+        currentIndex = 0; 
+    }
+
+    slides[currentIndex].classList.add("on"); 
+    updateEllipse()
+}
+
+updateEllipse()
